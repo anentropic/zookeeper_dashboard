ca4b71da1ad585a8b2b9f62dcebd4ffa00a2d8f3
Barry Allard <barry@barryallard.name>
2012-05-28 18:26:58 -0700

    cleanup manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-#try:
-#import settings # Assumed to be in the same directory.
-#except ImportError:
-#    import sys
-#    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r.")
-#    sys.exit(1)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zookeeper_dashboard.settings")
